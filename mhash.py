a0e8692e04ac87cd02c50bb7411e7d46bbcba4cf
Emil105 <68273811+Emil105105@users.noreply.github.com>
2022-03-26 11:22:44 +0100

    added documentation

--- mhash.py
+++ mhash.py
@@ -1,6 +1,12 @@
 
 
 def sha256(obj: bytes) -> bytes:
+    """
+    hash any bytes with sha3-256
+    
+    :param obj: bytes object
+    :return: the hash
+    """
     from hashlib import sha3_256
     h = sha3_256()
     h.update(obj)
@@ -8,6 +14,12 @@ def sha256(obj: bytes) -> bytes:
 
 
 def sha512(obj: bytes) -> bytes:
+    """
+    hash any bytes with sha3-512
+
+    :param obj: bytes object
+    :return: the hash
+    """
     from hashlib import sha3_512
     h = sha3_512()
     h.update(obj)
