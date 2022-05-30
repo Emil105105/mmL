3f0a7f2189fe5dd159619b79592f6e1fc0e27d77
Emil105 <68273811+Emil105105@users.noreply.github.com>
2022-05-30 14:29:33 +0200

    added a custom hash function

--- mhash.py
+++ mhash.py
@@ -3,7 +3,7 @@
 def sha256(obj: bytes) -> bytes:
     """
     hash any bytes with sha3-256
-    
+
     :param obj: bytes object
     :return: the hash
     """
@@ -24,3 +24,25 @@ def sha512(obj: bytes) -> bytes:
     h = sha3_512()
     h.update(obj)
     return h.digest()
+
+
+def any_hash(obj: bytes, length: int = 16, iterations: int = 1) -> bytes:
+    """
+    hash any bytes to any length with a modified version of sha3-512
+
+    :param obj: bytes object
+    :param length: length in bytes of the hash
+    :param iterations: number of iterations
+    :return: the hash
+    """
+    try:
+        from mmath import bytes_xor
+    except (ImportError, ModuleNotFoundError):
+        from mmL.mmath import bytes_xor
+    h = b''
+    for i in range(iterations):
+        h = sha512(obj)
+        while len(h) < length:
+            h += sha512(bytes_xor(sha512(obj), h, True))
+        obj = h
+    return h[0:length]
